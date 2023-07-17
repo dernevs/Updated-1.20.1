e62e659a72ed9999aa573d091defd1f4a7ba4149
BuildTools <unconfigured@null.spigotmc.org>
2023-07-17 15:50:41 +0200

    Initial commit

---
+++ src/main/java/net/dernevs/updated/UpdatedModClient.java
@@ -0,0 +1,11 @@
+package net.dernevs.updated;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UpdatedModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/dernevs/updated/UpdatedModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dernevs.updated;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UpdatedModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
